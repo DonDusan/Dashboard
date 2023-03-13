176f7c05e6af39565c72895a5a3a35d09ba37b7f
DonDusan <116301260+DonDusan@users.noreply.github.com>
2023-03-13 23:53:57 +0100

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName: "Foldable Mini Drone",
+        productNumber: "74318",
+        paymentStatus: "Due",
+        shipping: "Pending",
+    },
+    {
+        productName: "LARVENDER KF102 Drone",
+        productNumber: "54567",
+        paymentStatus: "Refunded",
+        shipping: "Declined",
+    },
+    {
+        productName: "Ruko F11 Pro Drone",
+        productNumber: "65989",
+        paymentStatus: "Due",
+        shipping: "Pending",
+    },
+    {
+        productName: "Drone with Camera Drone",
+        productNumber: "67097",
+        paymentStatus: "Paid",
+        shipping: "Delivered",
+    },
+    {
+        productName: "GPS 4k Drone",
+        productNumber: "22816",
+        paymentStatus: "Paid",
+        shipping: "Delivered",
+    },
+    {
+        productName: "DJI Air PRO 7S Model",
+        productNumber: "00506",
+        paymentStatus: "Due",
+        shipping: "Pending",
+    },
+    {
+        productName: "Lorenco Drone Gusta",
+        productNumber: "09945",
+        paymentStatus: "Paid",
+        shipping: "Delivered",
+    },
+];
